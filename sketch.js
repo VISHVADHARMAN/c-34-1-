faf944f570a89f8a944e187f38ed0e112bc4e5f4
VISHVADHARMAN <67373113+VISHVADHARMAN@users.noreply.github.com>
2020-07-30 19:50:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,56 +1,54 @@
-var ball, database;
+var hypnoticBall, database;
 var position;
 
-function setup(){
-    database = firebase.database();
-    //console.log(database);
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
 
-    var ballPosition = database.ref('ball/position');
-    ballPosition.on("value",readPosition,showError);
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  hypnoticBall = createSprite(250,250,10,10);
+  hypnoticBall.shapeColor = "red";
+
+
+  var hypnoticBallPosition = database.ref('ball/position');
+  hypnoticBallPosition.on("value", readPosition, showError);
 }
 
 function draw(){
-    background("white");
-    //console.log(position);
-    if(position !== undefined){
-
-    
+  background("white");
+ 
     if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
+      writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
+      writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
+      writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
+      writePosition(0,+1);
     }
-}
     drawSprites();
-  
+ 
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+  database.ref('ball/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
 }
+
 function readPosition(data){
-    position = data.val();
-    console.log(position.x);
-    ball.x = position.x;
-    ball.y = position.y;
+  position = data.val();
+  console.log(position.x);
+  hypnoticBall.x = position.x;
+  hypnoticBall.y = position.y;
 }
+
 function showError(){
-    console.log("error in writing to the database");
+  console.log("Error in writing to the database");
 }
-function writePosition(x,y){ 
-    database.ref('ball/position').set({ 
-        'x': position.x + x , 
-        'y': position.y + y 
-    }) 
-}
+
